78ce91ce842b8e08cd36ca5ba55ccfc979bf82e1
Kaylaesmith1 <kaylaesmith1@gmail.com>
2020-12-07 13:39:09 -0600

    Finalized CSS and JS

--- index.js
+++ index.js
@@ -25,23 +25,33 @@ currentTime.innerHTML = `${now} ${hours}:${minutes}`;
 
 function convertToCelcius(event) {
   event.preventDefault();
-
   let temperatureElement = document.querySelector("#tempNow");
-  let temperature = temperatureElement.innerHTML;
-  temperatureElement.innerHTML = Math.round(((temperature - 32) * 5) / 9);
+  let celciusTemperature = ((fahrenheitTemperature - 32) * 5) / 9;
+  temperatureElement.innerHTML = Math.round(celciusTemperature);
 
-  temperature = Number(temperature);
+  let celciusFeelsElement = document.querySelector("#feels");
+  let celciusTemperatureFeels = ((feels - 32) * 5) / 9;
+  celciusFeelsElement.innerHTML = Math.round(celciusTemperatureFeels);
+
+  fahrenheitLink.classList.remove("active");
+  celciusLink.classList.add("active");
 }
 
 function convertToFahrenheit(event) {
   event.preventDefault();
-
   let temperatureElement = document.querySelector("#tempNow");
-  let temperature = temperatureElement.innerHTML;
-  temperatureElement.innerHTML = Math.round((temperature * 9) / 5 + 32);
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 
-  temperature = Number(temperature);
+  let fahrenheitFeelsElement = document.querySelector("#feels");
+  let fahrenheitTemperatureFeels = feels;
+  fahrenheitFeelsElement.innerHTML = Math.round(fahrenheitTemperatureFeels);
+
+  fahrenheitLink.classList.add("active");
+  celciusLink.classList.remove("active");
 }
+
+let fahrenheitTemperature = null;
+let feels = null;
 
 let celciusLink = document.querySelector("#celcius-link");
 celciusLink.addEventListener("click", convertToCelcius);
@@ -50,15 +60,23 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
 function currentWeather(response) {
-  let currentTemperature = Math.round(response.data.main.temp);
-  let currentTemperatureElement = document.querySelector("#tempNow");
-  currentTemperatureElement.innerHTML = `${currentTemperature}`;
+  fahrenheitTemperature = response.data.main.temp;
+  let fahrenheitTemperatureElement = document.querySelector("#tempNow");
+  fahrenheitTemperatureElement.innerHTML = Math.round(
+    `${fahrenheitTemperature}`
+  );
   let description = response.data.weather[0].description;
   let humidity = response.data.main.humidity;
   let wind = Math.round(response.data.wind.speed);
-  let feels = Math.round(response.data.main.feels_like);
-  let sunrise = response.data.sys.sunrise;
-  let sunset = response.data.sys.sunset;
+  feels = Math.round(response.data.main.feels_like);
+  let unix_sunrise = response.data.sys.sunrise;
+  let date = new Date(unix_sunrise * 1000);
+  let hours = date.getHours();
+  let minutes = "0" + date.getMinutes();
+  let unix_sunset = response.data.sys.sunset;
+  let dateSunset = new Date(unix_sunset * 1000);
+  let hoursSunset = dateSunset.getHours();
+  let minutesSunset = "0" + dateSunset.getMinutes();
 
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = `${description}`;
@@ -67,11 +85,19 @@ function currentWeather(response) {
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = `${wind} mph`;
   let feelsLikeElement = document.querySelector("#feels");
-  feelsLikeElement.innerHTML = `${feels}°`;
+  feelsLikeElement.innerHTML = `${feels}`;
+  let formattedTimeSunrise = hours + ":" + minutes.substr(-2);
   let sunriseElement = document.querySelector("#sunrise");
-  sunriseElement.innerHTML = `${sunrise}`;
+  sunriseElement.innerHTML = `${formattedTimeSunrise}`;
+  let formattedTimeSunset = hoursSunset + ":" + minutesSunset.substr(-2);
   let sunsetElement = document.querySelector("#sunset");
-  sunsetElement.innerHTML = `${sunset}`;
+  sunsetElement.innerHTML = `${formattedTimeSunset}`;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 
   let city = document.querySelector("h1");
   city.innerHTML = `${response.data.name}`.toUpperCase();
@@ -108,3 +134,248 @@ function getCurrentPosition() {
 }
 let searchCurrent = document.querySelector("#search-current");
 searchCurrent.addEventListener("click", getCurrentPosition);
+
+function RecentlySearchedBCN(response) {
+  let maxTemperatureBCN = Math.round(response.data.main.temp_max);
+  let maxTemperatureElementBCN = document.querySelector("#max-temp-BCN");
+  maxTemperatureElementBCN.innerHTML = `${maxTemperatureBCN}°`;
+
+  let minTemperatureBCN = Math.round(response.data.main.temp_min);
+  let minTemperatureElementBCN = document.querySelector("#min-temp-BCN");
+  minTemperatureElementBCN.innerHTML = `${minTemperatureBCN}°`;
+  let iconElementBCN = document.querySelector("#iconBCN");
+  iconElementBCN.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElementBCN.setAttribute("alt", response.data.weather[0].description);
+}
+
+function RecentWeatherBCN(event) {
+  event.preventDefault();
+  let apiKey = "c5ec52f93b5c1b4b6fe696ad9119316f";
+  let units = "imperial";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrlRecentBCN = `${apiEndpoint}?q=Barcelona&appid=${apiKey}&units=${units}`;
+
+  axios.get(`${apiUrlRecentBCN}`).then(RecentlySearchedBCN);
+}
+
+let searchButtonRecentBCN = document.querySelector("#searching");
+searchButtonRecentBCN.addEventListener("submit", RecentWeatherBCN);
+
+function RecentWeatherBCNCelcius(event) {
+  event.preventDefault();
+  let apiKey = "c5ec52f93b5c1b4b6fe696ad9119316f";
+  let units = "metric";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrlRecentBCN = `${apiEndpoint}?q=Barcelona&appid=${apiKey}&units=${units}`;
+
+  axios.get(`${apiUrlRecentBCN}`).then(RecentlySearchedBCN);
+}
+let celciusLinkBCN = document.querySelector("#celcius-link");
+celciusLinkBCN.addEventListener("click", RecentWeatherBCNCelcius);
+let fahrenheitLinkBCN = document.querySelector("#fahrenheit-link");
+fahrenheitLinkBCN.addEventListener("click", RecentWeatherBCN);
+
+function RecentlySearchedChicago(response) {
+  let maxTemperatureChicago = Math.round(response.data.main.temp_max);
+  let maxTemperatureElementChicago = document.querySelector(
+    "#max-temp-Chicago"
+  );
+  maxTemperatureElementChicago.innerHTML = `${maxTemperatureChicago}°`;
+
+  let minTemperatureChicago = Math.round(response.data.main.temp_min);
+  let minTemperatureElementChicago = document.querySelector(
+    "#min-temp-Chicago"
+  );
+  minTemperatureElementChicago.innerHTML = `${minTemperatureChicago}°`;
+  let iconElementChicago = document.querySelector("#iconChicago");
+  iconElementChicago.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElementChicago.setAttribute("alt", response.data.weather[0].description);
+}
+
+function RecentWeatherChicago(event) {
+  event.preventDefault();
+  let apiKey = "c5ec52f93b5c1b4b6fe696ad9119316f";
+  let units = "imperial";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiEndpoint}?q=Chicago&appid=${apiKey}&units=${units}`;
+
+  axios.get(`${apiUrl}`).then(RecentlySearchedChicago);
+}
+
+let searchButtonRecentChicago = document.querySelector("#searching");
+searchButtonRecentChicago.addEventListener("submit", RecentWeatherChicago);
+
+function RecentWeatherChicagoCelcius(event) {
+  event.preventDefault();
+  let apiKey = "c5ec52f93b5c1b4b6fe696ad9119316f";
+  let units = "metric";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrlRecentChicago = `${apiEndpoint}?q=Chicago&appid=${apiKey}&units=${units}`;
+
+  axios.get(`${apiUrlRecentChicago}`).then(RecentlySearchedChicago);
+}
+
+let celciusLinkChicago = document.querySelector("#celcius-link");
+celciusLinkChicago.addEventListener("click", RecentWeatherChicagoCelcius);
+let fahrenheitLinkChicago = document.querySelector("#fahrenheit-link");
+fahrenheitLinkChicago.addEventListener("click", RecentWeatherChicago);
+
+function RecentlySearchedBerlin(response) {
+  let maxTemperatureBerlin = Math.round(response.data.main.temp_max);
+  let maxTemperatureElementBerlin = document.querySelector("#max-temp-Berlin");
+  maxTemperatureElementBerlin.innerHTML = `${maxTemperatureBerlin}°`;
+
+  let minTemperatureBerlin = Math.round(response.data.main.temp_min);
+  let minTemperatureElementBerlin = document.querySelector("#min-temp-Berlin");
+  minTemperatureElementBerlin.innerHTML = `${minTemperatureBerlin}°`;
+  let iconElementBerlin = document.querySelector("#iconBerlin");
+  iconElementBerlin.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElementBerlin.setAttribute("alt", response.data.weather[0].description);
+}
+
+function RecentWeatherBerlin(event) {
+  event.preventDefault();
+  let apiKey = "c5ec52f93b5c1b4b6fe696ad9119316f";
+  let units = "imperial";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiEndpoint}?q=Berlin&appid=${apiKey}&units=${units}`;
+
+  axios.get(`${apiUrl}`).then(RecentlySearchedBerlin);
+}
+
+let searchButtonRecentBerlin = document.querySelector("#searching");
+searchButtonRecentBerlin.addEventListener("submit", RecentWeatherBerlin);
+
+function RecentWeatherBerlinCelcius(event) {
+  event.preventDefault();
+  let apiKey = "c5ec52f93b5c1b4b6fe696ad9119316f";
+  let units = "metric";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrlRecentBerlin = `${apiEndpoint}?q=Berlin&appid=${apiKey}&units=${units}`;
+
+  axios.get(`${apiUrlRecentBerlin}`).then(RecentlySearchedBerlin);
+}
+
+let celciusLinkBerlin = document.querySelector("#celcius-link");
+celciusLinkBerlin.addEventListener("click", RecentWeatherBerlinCelcius);
+let fahrenheitLinkBerlin = document.querySelector("#fahrenheit-link");
+fahrenheitLinkBerlin.addEventListener("click", RecentWeatherBerlin);
+
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+<div class="col-2">
+          <strong class="forecast-time">
+            ${formatHours(forecast.dt * 1000)}
+            </strong>
+            <img src="http://openweathermap.org/img/wn/${
+              forecast.weather[0].icon
+            }@2x.png"
+            />  
+            <div class="weather-forecast-temperature">  
+              <span class="high-temperature-f">
+              ${Math.round(forecast.main.temp_max)}°
+              </span> 
+              <span class="low-temperature-f">${Math.round(
+                forecast.main.temp_min
+              )}°</span>
+            </div>  
+      </div>
+      `;
+  }
+}
+
+function searchForecast(event) {
+  event.preventDefault();
+  let inputCity = document.querySelector(".search-city").value;
+  let apiKey = "c5ec52f93b5c1b4b6fe696ad9119316f";
+  let units = "imperial";
+  let apiEndpointForecast = "https://api.openweathermap.org/data/2.5/forecast";
+
+  let apiUrlForecast = `${apiEndpointForecast}?q=${inputCity}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrlForecast).then(displayForecast);
+}
+
+let searchButtonForecast = document.querySelector("#searching");
+searchButtonForecast.addEventListener("submit", searchForecast);
+
+function searchForecastCelcius(event) {
+  event.preventDefault();
+  let inputCity = document.querySelector(".search-city").value;
+  let apiKey = "c5ec52f93b5c1b4b6fe696ad9119316f";
+  let units = "metric";
+  let apiEndpointForecast = "https://api.openweathermap.org/data/2.5/forecast";
+
+  let apiUrlForecast = `${apiEndpointForecast}?q=${inputCity}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrlForecast).then(displayForecast);
+}
+
+let celciusLinkForecast = document.querySelector("#celcius-link");
+celciusLinkForecast.addEventListener("click", searchForecastCelcius);
+let fahrenheitLinkForecast = document.querySelector("#fahrenheit-link");
+fahrenheitLinkForecast.addEventListener("click", searchForecast);
+
+function myLocationForecast(position) {
+  let latForecast = position.coords.latitude;
+  let lonForecast = position.coords.longitude;
+
+  let apiKey = "c5ec52f93b5c1b4b6fe696ad9119316f";
+  let units = "imperial";
+  let apiEndpointForecastCurrent =
+    "https://api.openweathermap.org/data/2.5/forecast";
+
+  let apiUrlForecastCurrent = `${apiEndpointForecastCurrent}?lat=${latForecast}&lon=${lonForecast}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrlForecastCurrent).then(displayForecast);
+}
+
+function getCurrentPositionForecast() {
+  navigator.geolocation.getCurrentPosition(myLocationForecast);
+}
+let searchCurrentForecast = document.querySelector("#search-current");
+searchCurrentForecast.addEventListener("click", getCurrentPositionForecast);
+
+function myLocationForecastCelcius(position) {
+  let latForecastC = position.coords.latitude;
+  let lonForecastC = position.coords.longitude;
+
+  let apiKey = "c5ec52f93b5c1b4b6fe696ad9119316f";
+  let units = "metric";
+  let apiEndpointForecastCurrent =
+    "https://api.openweathermap.org/data/2.5/forecast";
+
+  let apiUrlForecastCurrent = `${apiEndpointForecastCurrent}?lat=${latForecastC}&lon=${lonForecastC}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrlForecastCurrent).then(displayForecast);
+}
+
+let celciusLinkForecastMyLoc = document.querySelector("#celcius-link");
+celciusLinkForecastMyLoc.addEventListener("click", myLocationForecastCelcius);
+let fahrenheitLinkForecastMyLoc = document.querySelector("#fahrenheit-link");
+fahrenheitLinkForecastMyLoc.addEventListener("click", myLocationForecast);
